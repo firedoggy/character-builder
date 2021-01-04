4e7f0e99c86396980dd5347625801837ee2eaf8f
Samuel <sammkenzie@gmail.com>
2021-01-04 16:01:54 -0600

    Created noted guide for building CharactersController

---
+++ app/controllers/characters_controller.rb
@@ -0,0 +1,32 @@
+class CharactersController < ApplicationController
+
+    #CREATE
+
+        #NEW
+        #make a get request to '/characters/new'
+
+        #CREATE
+        #make a post request to '/characters'
+
+    #READ
+
+        #INDEX
+        #make a get request to '/characters'
+
+        #SHOW
+        #make a get request to '/characters/:id'
+
+    #UPDATE
+
+        #EDIT
+        #make a get request to '/characters/:id/edit'
+
+        #UPDATE
+        #make a patch request to '/characters/:id'
+
+    #DESTROY
+
+        #DESTROY
+        #make a delete request to '/characters/:id' 
+
+end
