8c4f66fe6210b48bf84e0c02f536977b99e207cb
Samuel <sammkenzie@gmail.com>
2021-01-05 17:12:16 -0600

    Added an if else block to /signup to ensure proper account creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,1 +1,22 @@
 class UsersController < ApplicationController
+
+    get '/signup' do
+        erb :'users/signup.erb'
+    end
+
+    post '/signup' do
+        user = User.new(params)
+        
+        if user.username.empty? || user.password.empty?
+            @error = "Username or password is empty."
+            erb :'users/signup'
+        elsif User.find_by(username: user.username)
+            @error = "Account with that username already exists"
+            erb :'users/signup'
+        else
+            user.save
+            session[:user_id] = user.id
+            redirect '/characters'
+        end
+    end
+end
