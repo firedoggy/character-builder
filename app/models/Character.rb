dd383d4a59426b7e9b8b86f95e303b203dcb8679
Samuel <sammkenzie@gmail.com>
2021-01-05 16:41:54 -0600

    Created and implemented methods on controllers and models

--- app/models/Character.rb
+++ app/models/Character.rb
@@ -1,10 +1,15 @@
 class Character
 
-    attr_accessor :name
+    attr_accessor :name, :race, :theme, :clas, :bio
     @@all = []
 
-    def initialize()
+    def initialize(name, race, theme, clas, bio)
         @name = name
+        @race = race
+        @theme = theme
+        @clas = clas
+        @bio = bio
+        @@all << self
     end
 
     def self.all
--- app/models/User.rb
+++ app/models/User.rb
@@ -1,3 +1,15 @@
 class User
 
+    attr_accessor :username, :password
+    @@all = []
+
+    def initialize(username, password)
+        @username = username
+        @password = password
+        @@all << self
+    end
+
+    def self.all
+        return @@all
+    end
 end
